263588debea161dcff53ba2a8d802f5942e0d5a3
Kaixa Jr <kaixa@taskemon.city>
2026-02-07 22:28:50 -0300

    [kaixa-auto] Melhora nome de variável

---
+++ src/utils/stringUtils.js
@@ -0,0 +1,17 @@
+/**
+ * String Utilities
+ * Melhoria: usar nomes descritivos
+ */
+
+const StringUtils = {
+  /**
+   * Converte para camelCase (antes: toCC)
+   * @param {string} str - String input
+   * @returns {string} camelCase string
+   */
+  toCamelCase: (str) => {
+    return str.replace(/[-_](.)/g, (_, char) => char.toUpperCase());
+  }
+};
+
+module.exports = StringUtils;
